653996a957e7eff0fdfb369a6a12aad9ab89a143
Anna Ingels <annaingels@Annas-MacBook-Pro.local>
2017-09-19 17:07:47 -0700

    dayone done

--- day1/js/script.js
+++ day1/js/script.js
@@ -1,5 +1,18 @@
 window.addEventListener('keydown', function(e) {
- console.log(e.keyCode);
-
+ const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+ const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+   audio.currentTime = 0;
+   audio.play();
+   key.classList.add('playing');
 });
-s
+
+function removeTransistion(e) {
+  if(e.propertyName !== 'transform') return;
+  console.log(e.propertyName );
+  this.classList.removej('playing');
+};
+
+
+var keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransistion));
